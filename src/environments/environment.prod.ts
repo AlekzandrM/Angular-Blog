f32900e132541a174b4cea377a9aeb9f18bc1483
Aleksandr <mishenko.aleksandr@gmail.com>
2020-04-15 22:01:10 +0300

    created PostService for creating Posts and post them to backend FB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyCItldL6DsscFogBfxIJJ3u_4WdME117bU'
+  apiKey: 'AIzaSyCItldL6DsscFogBfxIJJ3u_4WdME117bU',
+  fbDbUrl: 'https://angular-blog-6b6c5.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyCItldL6DsscFogBfxIJJ3u_4WdME117bU'
+  apiKey: 'AIzaSyCItldL6DsscFogBfxIJJ3u_4WdME117bU',
+  fbDbUrl: 'https://angular-blog-6b6c5.firebaseio.com'
 };
 
 /*
