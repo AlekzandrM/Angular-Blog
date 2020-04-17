a76dcd46bef7ad56a275187f042d75a24c9a1025
Aleksandr <mishenko.aleksandr@gmail.com>
2020-04-17 23:45:57 +0300

    Post Removal. Added remove method in PostService, implemented in DashboardPage

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSup: Subscription
+  dSup: Subscription
   searchStr = ''
 
   constructor(private postsService: PostsService) {
@@ -27,9 +28,14 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSup) {
       this.pSup.unsubscribe()
     }
+    if (this.dSup) {
+      this.dSup.unsubscribe()
+    }
   }
 
   remove(id: string) {
-
+    this.dSup = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 }
